e2a1b9886ad80cfe61adb8d1a0f7abdbd525d1ef
Arun8077 <akssharma3273@gmail.com>
2025-10-26 04:07:02 +0530

    adding ec2-module

---
+++ resource-ec2/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create an EC2 instance
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Terraform-EC2-Example"
+  }
+}
---
+++ resource-ec2/variable.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region to deploy EC2"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-0c02fb55956c7d316"  # Amazon Linux 2 in ap-south-1
+}
